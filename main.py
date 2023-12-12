ffcb786361fcf876bb64c0073a98d5fb5c1f0905
Ly Nguyen <lyntc.elec@gmail.com>
2023-12-12 10:34:42 +0700

    Update

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import uvicorn
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
 
 def fastapi_app() -> FastAPI:
     app = FastAPI()
 
-    @app.get("/items/{item_id}")
-    async def get_item(item_id: int):
-        print(item_id)
+    @app.post("/items/{item_id}")
+    async def get_item(item_id: int, request: Request):
+        # Extract user ID and S/N from request body
+        body = await request.json()
+        user_id = body.get("userId")
+        sn = body.get("sn")
+
+        print(f"Item ID: {item_id}, User ID: {user_id}, S/N: {sn}")
 
         tts = ""
 
@@ -23,16 +28,17 @@ def fastapi_app() -> FastAPI:
             case 4:
                 tts = "Em đã mở chế độ ngủ trên máy lạnh, chúc anh ngủ ngon nhé"
             case _:
-                "xin chào"
+                tts = "Xin chào"
 
+        print({"userId": user_id, "maikaSNS": [sn], "tts": tts})
         requests.post(
             "https://staging.smarthome.iviet.com/api/v1/gateway/notification",
             headers={"Content-Type": "application/json"},
-            json={"userId": 1016, "maikaSNS": ["A1048540088"], "tts": tts},
+            json={"userId": int(user_id), "maikaSNS": [sn], "tts": tts},
         )
         return {}
-    
-    app.mount("/", StaticFiles(directory="frontend",html = True), name="static")
+
+    app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
 
     return app
 
@@ -42,7 +48,5 @@ app = fastapi_app()
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(3010),
+        app, host="0.0.0.0", port=int(3010),
     )
